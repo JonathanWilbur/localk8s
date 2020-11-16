af0623d0e38f61d88dd316b1e7d0d565bcbbb4a1
Jonathan M. Wilbur <jonathan@wilbur.space>
2020-11-16 11:50:20 -0500

    Implement fileserver and database server
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@ apt update
 apt upgrade -y
 apt install -y ansible
 ansible --version
-ansible-galaxy collection install community.general
-ansible-galaxy collection install ansible.posix
-ansible-galaxy collection install community.kubernetes
+ansible-galaxy collection install community.general ansible.posix community.kubernetes
