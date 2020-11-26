6f92b4de4c8a54f0a1a8e6209b09df9e5156c3cd
Jonathan M. Wilbur <jonathan@wilbur.space>
2020-11-25 23:24:33 -0500

    Update installs script
    Signed-off-by: Jonathan M. Wilbur <jonathan@wilbur.space>

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt update
 apt upgrade -y
-apt install -y ansible
+apt install -y ansible python3-pip
 ansible --version
 ansible-galaxy collection install community.general ansible.posix community.kubernetes
+pip3 install --upgrade requests
